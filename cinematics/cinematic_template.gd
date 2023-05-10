93cdc43cb7394b8a7609f30249d7a85068e8ef92
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-05-10 12:54:03 -0300

    Create template for cinematics

---
+++ cinematics/cinematic_template.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+signal cinematic_finished
+
+@onready var actors = get_node("Actors")
+@onready var camera = get_node("Camera2D")
+@onready var anim_player = get_node("AnimationPlayer")
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+
+
+func play_cinematic() -> void:
+	camera.enabled = true
+	anim_player.play("start")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "start":
+		cinematic_finished.emit()
