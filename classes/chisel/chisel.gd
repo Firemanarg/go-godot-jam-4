424ddb8439d5d4fdfe1acb00e183fe022afc2a5a
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-23 09:00:39 -0300

    chisel: Create chisel & chisel_selection_button

---
+++ classes/chisel/chisel.gd
@@ -0,0 +1,18 @@
+class_name Chisel
+extends Node
+
+
+@export var affected_blocks: Array[Vector2i] = []
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
---
+++ classes/chisel/chisel_selection_button.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
