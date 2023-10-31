3e5023bbd124d7987c8c7598c4cbd204e9ab0224
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-31 17:43:49 -0300

    cleanup: remove old comments

--- gui/main_screen.gd
+++ gui/main_screen.gd
@@ -1,9 +1,6 @@
 extends Control
 
 
-#signal button_play_pressed
-#signal button_settings_pressed
-#signal button_credits_pressed
 signal button_pressed(action_name: String)
 
 
@@ -21,18 +18,12 @@ func _physics_process(delta):
 
 
 func _on_button_play_pressed():
-#	button_play_pressed.emit()
 	button_pressed.emit("play")
-#	print("button_play_pressed")
 
 
 func _on_button_settings_pressed():
-#	button_settings_pressed.emit()
 	button_pressed.emit("settings")
-#	print("button_settings_pressed")
 
 
 func _on_button_credits_pressed():
-#	button_credits_pressed.emit()
 	button_pressed.emit("credits")
-#	print("button_credits_pressed")
