a0bb6234cb961685735c38ef7259767d6423665a
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-10-31 18:10:37 -0300

    textures: Change chisel button textures

--- classes/chisel/chisel_selection_button.gd
+++ classes/chisel/chisel_selection_button.gd
@@ -1,5 +1,9 @@
 extends TextureButton
 
+const HOVER_SCALE: float = 1.2
+const PRESSED_SCALE: float = 1.1
+const DEFAULT_MODULATE: Color = Color(1, 1, 1, 1)
+const PRESSED_MODULATE: Color = Color(0.6, 0.6, 0.6, 1)
 
 @export var chisel: Chisel = null
 
@@ -15,3 +19,23 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	pass
 
+
+func _on_mouse_entered() -> void:
+	scale = Vector2.ONE * HOVER_SCALE
+
+
+func _on_mouse_exited() -> void:
+	scale = Vector2.ONE
+
+
+#func _on_pressed() -> void:
+#	scale = Vector2.ONE
+#	modulate = PRESSED_MODULATE
+
+
+func _on_button_down() -> void:
+	modulate = PRESSED_MODULATE
+
+
+func _on_button_up() -> void:
+	modulate = DEFAULT_MODULATE
